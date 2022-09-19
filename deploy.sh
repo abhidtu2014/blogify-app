5422b3a22265799e08f3313533087dc02ab3a234
Abhishek Rajput <abhishek.rajput@fluxon.com>
2022-09-19 17:18:13 +0530

    Complete: Cloud run deployment with shell script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# deploy.sh
+#!/bin/bash
+set -e
+export $(xargs < .env)
+# Image tag is the last commit hash
+IMAGE_TAG=`git rev-parse HEAD`
+
+docker build . -t $REPO_NAME:$IMAGE_TAG
+docker tag $REPO_NAME $HOSTNAME/$GCP_PROJECT_NAME/$REPO_NAME:$IMAGE_TAG
+docker push $HOSTNAME/$GCP_PROJECT_NAME/$REPO_NAME:$IMAGE_TAG
+gcloud run deploy $REPO_NAME --image $HOSTNAME/$GCP_PROJECT_NAME/$REPO_NAME:$IMAGE_TAG \
+  --project $GCP_PROJECT_NAME \
+  --region $GCP_REGION \
